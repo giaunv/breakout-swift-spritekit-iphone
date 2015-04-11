95c6d37da353d8a3a911c055a050a326602e3367
Giau Ngo <giaunv@gmail.com>
2015-04-11 12:01:17 +0700

    Project Setup

---
+++ breakout-swift-spritekit/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  breakout-swift-spritekit
+//
+//  Created by giaunv on 4/11/15.
+//  Copyright (c) 2015 366. All rights reserved.
+//
+
+import SpriteKit
+
+let BallCategoryName = "ball"
+let PaddleCategoryName = "paddle"
+let BlockCategoryName = "block"
+let BlockNodeCategoryName = "blockNode"
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        super.didMoveToView(view);
+    }
+}
