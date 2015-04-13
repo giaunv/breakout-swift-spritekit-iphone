026da78ec6a53de66b0da0cf23c6c6cf0597ee0b
quangvu0702 <quangvu0702@yahoo.com>
2015-04-14 06:36:09 +0700

    Adding a Game Over Scene

---
+++ breakout-swift-spritekit/GameOverScene.swift
@@ -0,0 +1,27 @@
+//
+//  GameOverScene.swift
+//  breakout-swift-spritekit
+//
+//  Created by MAC on 4/14/15.
+//  Copyright (c) 2015 366. All rights reserved.
+//
+
+import SpriteKit
+
+let GameOverLabelCategoryName = "gameOverLabel"
+
+class GameOverScene: SKScene {
+    var gameWon : Bool = false {
+        didSet {
+            let gameOverLabel = childNodeWithName(GameOverLabelCategoryName) as SKLabelNode!
+            gameOverLabel.text = gameWon ? "Game Won" : "Game Over"
+        }
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        if let view = view {
+            let gameScene = GameScene.unarchiveFromFile("GameScene") as GameScene!
+            view.presentScene(gameScene)
+        }
+    }
+}
